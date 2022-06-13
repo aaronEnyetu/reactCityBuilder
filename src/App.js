ec199b8d5fbb566a7b3b28d9a006e182f37d8a39
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-13 12:38:25 -0700

    Dropdowns working

--- src/App.js
+++ src/App.js
@@ -9,18 +9,17 @@ import CityNameInput from './CityNameInput';
 import City from './City';
 
 
-const [Skyline, setSkyline] = useState('Busy');
-const [waterfront, setWaterfront] = useState('Sunset');
-const [castle, setCastle] = useState('Mysterious');
-
-const [sloganlist, setSloganlist] = useState('');
 
 function App() {
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
   // you'll need to track a city name, which starts as the city name of your choice.
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
-
+  const [skyline, setSkyline] = useState('Busy');
+  const [waterfront, setWaterfront] = useState('Sunset');
+  const [castle, setCastle] = useState('Mysterious');
+  
+  const [sloganlist, setSloganlist] = useState('');
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
@@ -42,6 +41,7 @@ function App() {
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
         {/* here, the SloganList component takes the array of slogans that lives in state */}
+
 
       </div>
     </div>
